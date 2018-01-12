4d861a8163f210dab5a6a836bc842f1a12c5bc26
RobPiwowarek <robpiwowarek@gmail.com>
2018-01-12 12:16:22 +0100

    init broadcaster, filemanager, protocols in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <conn/TCPServer.h>
 #include "ui/UI.h"
+#include "files/FileManager.h"
 
 using namespace ui;
 using namespace std;
@@ -7,6 +8,12 @@ using namespace std;
 int main()
 {
 	conn::TCPConnection::enableConnections();
+    conn::IPv4Address local = conn::IPv4Address::getLocalAddress(0);
+    conn::UDPBroadcaster udpBroadcaster(conn::IPv4Address::getBroadcastAddress(local, 24, 8080));
+
+    files::FileManager fileManager(local, "");
+
+    proto::Protocols::init(&udpBroadcaster, &fileManager);
 
     UI userInterface = UI();
 
