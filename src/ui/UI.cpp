a762edf9946732efe4e49972bdcd3ed85348a329
lychanl <lycha_@poczta.onet.pl>
2018-01-15 20:08:19 +0100

    fix packages

--- src/ui/UI.cpp
+++ src/ui/UI.cpp
@@ -214,7 +214,7 @@ int UI::Parser::downloadFile(string fileSrc, string fileDst)
             string path = fileDst.substr(0, fileDst.length() - strlen(base));
 
 
-            system(std::strncat(const_cast<char *>("mkdir -p "), path.c_str(), path.length() + 1));
+            system(("mkdir -p "+ path).c_str());
 
             std::vector<files::Descriptor> local = fileManager->listLocalFiles();
 
